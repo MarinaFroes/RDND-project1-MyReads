04aafdbb9df396bb4edf8d0602fba05831054775
MFAC <ninafroes@gmail.com>
2019-11-01 15:31:55 +0100

    feat: Generate Shelf for each Shelf type

--- src/Components/Shelves.js
+++ src/Components/Shelves.js
@@ -2,18 +2,18 @@ import React from 'react'
 import Shelf from './Shelf'
 
 const SHELVES_TYPES = [
-  'Currently reading',
-  'Want to read',
-  'Read'
+  ['Currently Reading','currentlyReading'],
+  ['Want to read','wantToRead'],
+  ['Read','read']
 ]
 
 function Shelves({ booksList }) {
- 
+  
   return (
     <div>
       {
         SHELVES_TYPES.map( shelf => (
-          <Shelf shelf={shelf} booksList={booksList} key={shelf}/>
+          <Shelf title={shelf[0]} shelf={shelf[1]} booksList={booksList} key={shelf}/>
         ))
       }
     </div>
