0e38fcf30b77c8ad88d1a7c70d89a477c3ffffc7
MFAC <ninafroes@gmail.com>
2019-11-01 14:57:45 +0100

    feat: Add basic book component

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-function Books({ props }) {
+function Books({ booksList }) {
   return (
     <div>
-      
+      {
+        booksList.map(book => (
+          <div key={book.id} className='book-info'>
+            <img src={book.imageLinks.smallThumbnail} alt=''/>
+            <p>Title: {book.title}</p>
+            {book.subtitle && <p>Subtitle: {book.subtitle}</p>}
+            <p>Authors: {book.authors}</p>
+            <p>Rate: {book.averageRating || "No rating"}</p>
+            <p>Shelf: {book.shelf}</p>
+          </div>
+        ))
+      }
+   
     </div>
   )
 }
