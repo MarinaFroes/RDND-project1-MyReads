bcd7e8fd20ca948993f857cbdcede7f99da10dad
MFAC <ninafroes@gmail.com>
2019-11-01 16:26:30 +0100

    feat: Add AddBar component

---
+++ src/Components/AddBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import add from '../icons/add.svg'
+
+function AddBar() {
+  return (
+    <React.Fragment>
+      <img src={`${add}`} alt='' className='add-icon' />
+    </React.Fragment>
+  )
+}
+
+export default AddBar;
