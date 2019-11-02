17a3179112945060ef682be6e80e191a59e502c2
MFAC <ninafroes@gmail.com>
2019-11-02 11:17:48 +0100

    feat: Add onUpdateBook prop

--- src/Components/Shelf.js
+++ src/Components/Shelf.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Book from './Book'
 
-function Shelf({ title, shelf, booksList }) {
+function Shelf({ title, shelf, booksList, onUpdateBook }) {
   return (
     <div className='shelf'>
       <h1 className='shelf-title'>{title}</h1>
       <div className='books-container'>
-        {booksList.map(book => book.shelf === shelf && <Book key={book.id} book={book} />)}
+        {booksList.map(book => book.shelf === shelf && <Book key={book.id} book={book} onUpdateBook={onUpdateBook}/>)}
       </div>
     </div>
   )
