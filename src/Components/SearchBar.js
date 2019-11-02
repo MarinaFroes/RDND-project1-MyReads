9f9d24cc72eed023dccdf31517ce75c5f8eed609
MFAC <ninafroes@gmail.com>
2019-11-02 16:15:27 +0100

    feat: Lift state up

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,29 +1,46 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import magnifyingglass from '../icons/magnifyingglass.svg'
 
 class SearchBar extends Component {
-  state = {
-    userInput: ''
+  static propTypes = {
+    query: PropTypes.string.isRequired,
+    onUpdateQuery: PropTypes.func.isRequired
   }
 
-  handleSubmit = (e) => {
-    e.preventDefault()
-  }
+  // state = {
+  //   query: ''
+  // }
+
+  // handleSubmit = e => {
+  //   e.preventDefault();
+  //   const target = e.target;
+  //   console.log(target)
+  // }
 
   render() {
+    // const { query } = this.state
+    const { query, onUpdateQuery } = this.props
+  
     return (
+      <>
       <form onSubmit={this.handleSubmit}>
+        <img
+          src={`${magnifyingglass}`}
+          alt=''
+          className='magnifying-glass-icon'
+        />
         <input
           type='text'
           name='search-bar'
           id='search-bar'
-          value={this.state.userInput}
-          placeholder='Search' />
-        <input
-          type='submit'
-          value='Submit'
-          className='input-submit'
+          value={query}
+          placeholder='Search book...'
+          onChange={event => onUpdateQuery(event.target.value)}
         />
-      </form>
+        </form>
+        <p>{query}</p>
+      </>
     )
   }
 }
