51b27f301ff6b1aa1b3c9fe6671f6323c16f4b8e
MFAC <ninafroes@gmail.com>
2019-11-02 11:31:03 +0100

    refactor: Change div and p to ul and li and add role attribute

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -4,12 +4,18 @@ import { SHELVES_TYPES } from '../utils/SHELVES_TYPES'
 
 function DropDown() {
   return (
-    <div className='drop-down-menu'>
+    <ul className='drop-down-menu' role="menubar">
       {/* <img src={`${dropdown}`} alt='' className='dropdown-icon' /> */}
       {SHELVES_TYPES.map(shelf => ( 
-        <p key={shelf[1]} id={shelf[1]}>{shelf[0]}</p>
+        <li
+          key={shelf[1]}
+          id={shelf[1]}
+          role='menuitem'
+        >
+          {shelf[0]}
+        </li>
       ))}
-    </div>
+    </ul>
   )
 }
 
