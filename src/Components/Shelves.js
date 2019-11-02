91af2fd0eac2fcf25aa003288832672b2b0a699e
MFAC <ninafroes@gmail.com>
2019-11-02 11:16:52 +0100

    feat: Import SHELVES_TYPES

--- src/Components/Shelves.js
+++ src/Components/Shelves.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import Shelf from './Shelf'
+import { SHELVES_TYPES } from '../utils/SHELVES_TYPES'
 
-const SHELVES_TYPES = [
-  ['Currently Reading','currentlyReading'],
-  ['Want to read','wantToRead'],
-  ['Read','read']
-]
-
-function Shelves({ booksList }) {
+function Shelves({ booksList, onUpdateBook }) {
   
   return (
     <div>
       {
         SHELVES_TYPES.map( shelf => (
-          <Shelf title={shelf[0]} shelf={shelf[1]} booksList={booksList} key={shelf}/>
+          <Shelf title={shelf[0]} shelf={shelf[1]} booksList={booksList} key={shelf} onUpdateBook={onUpdateBook}/>
         ))
       }
     </div>
