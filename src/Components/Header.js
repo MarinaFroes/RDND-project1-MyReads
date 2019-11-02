2358ee340cddca858be266aadb117a5fd4ab9cab
MFAC <ninafroes@gmail.com>
2019-11-02 16:12:32 +0100

    feat: Add PropTypes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function Header({ title }) {
   return (
@@ -9,3 +10,7 @@ function Header({ title }) {
 }
 
 export default Header;
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+}
