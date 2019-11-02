426879707b38b01011e9d42bba57d90c353f2527
MFAC <ninafroes@gmail.com>
2019-11-02 17:45:17 +0100

    feat: Pass book as prop to DropDown

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -32,13 +32,13 @@ class Book extends Component {
             className='dropdown-icon'
             onClick={() => this.setState({ clicked: !this.state.clicked })}
           />
-          {this.state.clicked && <DropDown onUpdateBook={onUpdateBook}/>}
+          {this.state.clicked && <DropDown book={book} onUpdateBook={onUpdateBook}/>}
         </div>
 
         <h2 className='book-title'>{book.title}</h2>
         {/* {book.subtitle && <p>Subtitle: {book.subtitle}</p>} */}
         <p><strong>Authors:</strong> {book.authors}</p>
-        <p><strong>Rate:</strong> {book.averageRating || "No rating"}</p>
+        {/* <p><strong>Rate:</strong> {book.averageRating || "No rating"}</p> */}
         <p>Shelf: {book.shelf}</p>
       </div>
     )
