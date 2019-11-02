92554c995b88199570a3b0b443c89553a9c3eb5f
MFAC <ninafroes@gmail.com>
2019-11-02 16:12:15 +0100

    feat: Add PropTypes

--- src/Components/Shelf.js
+++ src/Components/Shelf.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Book from './Book'
 
 function Shelf({ title, shelf, booksList, onUpdateBook }) {
@@ -13,3 +14,10 @@ function Shelf({ title, shelf, booksList, onUpdateBook }) {
 }
 
 export default Shelf;
+
+Shelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  shelf: PropTypes.string.isRequired,
+  booksList: PropTypes.array.isRequired,
+  onUpdateBook: PropTypes.func.isRequired
+}
