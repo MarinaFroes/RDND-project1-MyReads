a16a0f04b0c400b2d8cc0b3b602ab280901f7079
MFAC <ninafroes@gmail.com>
2019-11-02 11:18:13 +0100

    feat: Import DropDown menu and receive onUpdateBook prop

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,21 +1,55 @@
-import React from 'react'
+// import React from 'react'
+import React, { Component } from 'react'
 import dropdown from '../icons/dropdown.svg'
+import DropDown from './DropDown'
 
-function Book({ book }) {
-  return (
-    <div className='book-info'>
-      <div className='cover-container'>
-        <img src={book.imageLinks.smallThumbnail} alt='' />
-        <img src={`${dropdown}`} alt='' className='dropdown-icon' />
+class Book extends Component {
+  state = {
+    clicked: false
+  }
+
+  render() {
+    const { book, onUpdateBook } = this.props;
+
+    return (
+      <div className='book-info'>
+        <div className='cover-container'>
+          <img src={book.imageLinks.smallThumbnail} alt='' />
+          <img
+            src={`${dropdown}`}
+            alt=''
+            className='dropdown-icon'
+            onClick={() => this.setState({ clicked: !this.state.clicked })}
+          />
+          {this.state.clicked && <DropDown onUpdateBook={onUpdateBook}/>}
+        </div>
+
+        <h2 className='book-title'>{book.title}</h2>
+        {/* {book.subtitle && <p>Subtitle: {book.subtitle}</p>} */}
+        <p><strong>Authors:</strong> {book.authors}</p>
+        <p><strong>Rate::</strong> {book.averageRating || "No rating"}</p>
+        <p>Shelf: {book.shelf}</p>
       </div>
-      
-      <h2 className='book-title'>{book.title}</h2>
-      {/* {book.subtitle && <p>Subtitle: {book.subtitle}</p>} */}
-      <p>Authors: {book.authors}</p>
-      <p>Rate: {book.averageRating || "No rating"}</p>
-      <p>Shelf: {book.shelf}</p>
-    </div>
-  )
+    )
+  }
 }
+
+// function Book({ book, onUpdateBook }) {
+//   return (
+//     <div className='book-info'>
+//       <div className='cover-container'>
+//         <img src={book.imageLinks.smallThumbnail} alt='' />
+//         <img src={`${dropdown}`} alt='' className='dropdown-icon' onClick={onUpdateBook} />
+//         <DropDown />
+//       </div>
+      
+//       <h2 className='book-title'>{book.title}</h2>
+//       {/* {book.subtitle && <p>Subtitle: {book.subtitle}</p>} */}
+//       <p><strong>Authors:</strong> {book.authors}</p>
+//       <p><strong>Rate::</strong> {book.averageRating || "No rating"}</p>
+//       <p>Shelf: {book.shelf}</p>
+//     </div>
+//   )
+// }
 
 export default Book
