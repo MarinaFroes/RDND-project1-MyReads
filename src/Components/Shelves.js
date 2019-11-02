680273a6a27ad69d79d41f9632c6a42eb8f1c8ed
MFAC <ninafroes@gmail.com>
2019-11-02 12:24:24 +0100

    refactor: Shelf attributes

--- src/Components/Shelves.js
+++ src/Components/Shelves.js
@@ -3,12 +3,17 @@ import Shelf from './Shelf'
 import { SHELVES_TYPES } from '../utils/SHELVES_TYPES'
 
 function Shelves({ booksList, onUpdateBook }) {
-  
   return (
     <div>
       {
         SHELVES_TYPES.map( shelf => (
-          <Shelf title={shelf[0]} shelf={shelf[1]} booksList={booksList} key={shelf} onUpdateBook={onUpdateBook}/>
+          <Shelf 
+            title={shelf[0]} 
+            shelf={shelf[1]} 
+            booksList={booksList} 
+            key={shelf} 
+            onUpdateBook={onUpdateBook}
+          />
         ))
       }
     </div>
