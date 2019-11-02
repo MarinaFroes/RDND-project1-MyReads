6b8e40f4c639031847e4e7e0edba85d57929c02c
MFAC <ninafroes@gmail.com>
2019-11-02 10:09:44 +0100

    styles: Add className shelf-title

--- src/Components/Shelf.js
+++ src/Components/Shelf.js
@@ -4,7 +4,7 @@ import Book from './Book'
 function Shelf({ title, shelf, booksList }) {
   return (
     <div className='shelf'>
-      <h1>{title}</h1>
+      <h1 className='shelf-title'>{title}</h1>
       <div className='books-container'>
         {booksList.map(book => book.shelf === shelf && <Book key={book.id} book={book} />)}
       </div>
