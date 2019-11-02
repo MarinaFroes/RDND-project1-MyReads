6318019b95dda08f81b61e15a22e8aa127b7845e
MFAC <ninafroes@gmail.com>
2019-11-02 16:30:33 +0100

    refactor: Turn class into functional component

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,30 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import magnifyingglass from '../icons/magnifyingglass.svg'
 
-class SearchBar extends Component {
-  static propTypes = {
-    query: PropTypes.string.isRequired,
-    onUpdateQuery: PropTypes.func.isRequired
-  }
-
-  // state = {
-  //   query: ''
-  // }
-
-  // handleSubmit = e => {
-  //   e.preventDefault();
-  //   const target = e.target;
-  //   console.log(target)
-  // }
-
-  render() {
-    // const { query } = this.state
-    const { query, onUpdateQuery } = this.props
-  
-    return (
-      <>
-      <form onSubmit={this.handleSubmit}>
+function SearchBar({ query, onUpdateQuery }) {
+  return (
+    <>
+      <form>
         <img
           src={`${magnifyingglass}`}
           alt=''
@@ -35,15 +16,49 @@ class SearchBar extends Component {
           name='search-bar'
           id='search-bar'
           value={query}
-          placeholder='Search book...'
+          placeholder='Search book title...'
           onChange={event => onUpdateQuery(event.target.value)}
         />
-        </form>
-        <p>{query}</p>
-      </>
-    )
-  }
+      </form>
+      <p>{query}</p>
+    </>
+  )
 }
+
+// class SearchBar extends Component {
+//   static propTypes = {
+//     query: PropTypes.string.isRequired,
+//     onUpdateQuery: PropTypes.func.isRequired
+//   }
+//   render() {
+//     // const { query } = this.state
+//     const { query, onUpdateQuery } = this.props
+//     return (
+//       <>
+//       <form onSubmit={this.handleSubmit}>
+//         <img
+//           src={`${magnifyingglass}`}
+//           alt=''
+//           className='magnifying-glass-icon'
+//         />
+//         <input
+//           type='text'
+//           name='search-bar'
+//           id='search-bar'
+//           value={query}
+//           placeholder='Search book title...'
+//           onChange={event => onUpdateQuery(event.target.value)}
+//         />
+//         </form>
+//         <p>{query}</p>
+//       </>
+//     )
+//   }
+// }
 
 export default SearchBar;
 
+SearchBar.propTypes = {
+  query: PropTypes.string.isRequired,
+  onUpdateQuery: PropTypes.func.isRequired
+}
