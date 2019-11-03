574abed42112c4be89fc70596771dc09f8360ff3
MFAC <ninafroes@gmail.com>
2019-11-03 16:16:11 +0100

    refactor: Delete query paragraph

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -5,26 +5,23 @@ import { Link } from 'react-router-dom'
 
 function SearchBar({ query, onUpdateQuery }) {
   return (
-    <>
-      <form>
-        <Link to='/'>
-          <img
-            src={`${arrowback}`}
-            alt=''
-            className='arrowback-icon'
-          />
-        </Link>
-        <input
-          type='text'
-          name='search-bar'
-          className='search-bar'
-          value={query}
-          placeholder='Search book title or author...'
-          onChange={event => onUpdateQuery(event.target.value)}
+    <form>
+      <Link to='/'>
+        <img
+          src={`${arrowback}`}
+          alt=''
+          className='arrowback-icon'
         />
-      </form>
-      <p>{query}</p>
-    </>
+      </Link>
+      <input
+        type='text'
+        name='search-bar'
+        className='search-bar'
+        value={query}
+        placeholder='Search book title or author...'
+        onChange={event => onUpdateQuery(event.target.value)}
+      />
+    </form>
   )
 }
 
