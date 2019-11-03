ec9095a0a61b7fdb7e06854f9dc226b530010af0
MFAC <ninafroes@gmail.com>
2019-11-03 16:11:40 +0100

    feat: Merge Shelves and Shelf components into a unique component

--- src/Components/Shelf.js
+++ src/Components/Shelf.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Book from './Book'
+import { SHELVES_TYPES } from '../utils/SHELVES_TYPES'
 
-function Shelf({ title, shelf, showingBooks, onUpdateBook }) {
+function Shelf({ showingBooks, onUpdateBook }) {
   return (
-    <div className='shelf'>
-      <h1 className='shelf-title'>{title}</h1>
-      <div className='books-container'>
-        {showingBooks.map(book => book.shelf === shelf && <Book key={book.id} book={book} onUpdateBook={onUpdateBook}/>)}
-      </div>
-    </div>
+    <section>
+      {
+        SHELVES_TYPES.map( shelf => (
+          <div key={shelf[1]} className='shelf'>
+            <h1 className='shelf-title'>{shelf[0]}</h1>
+            <div className='books-container'>
+              {showingBooks.filter(book => book.shelf === shelf[1]).map(book => <Book key={book.id} book={book} onUpdateBook={onUpdateBook}/>)}
+            </div>
+          </div>
+         )
+        )
+      }
+    </section>
   )
 }
 
 export default Shelf;
 
 Shelf.propTypes = {
-  title: PropTypes.string.isRequired,
-  shelf: PropTypes.string.isRequired,
   showingBooks: PropTypes.array.isRequired,
   onUpdateBook: PropTypes.func.isRequired
 }
+
