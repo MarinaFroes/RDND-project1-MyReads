9dbdf65e695fba21a3abaf92f3474e71c4b5ef71
MFAC <ninafroes@gmail.com>
2019-11-03 16:12:26 +0100

    feat: Turn class into functional component

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,68 +1,28 @@
-// import React from 'react'
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-// import dropdown from '../icons/dropdown.svg'
 import DropDown from './DropDown'
 
+function Book({ book, onUpdateBook }) {
+  return (
+    <section className='book-info'>
+      <div className='cover-container'>
+        <img
+          className='cover-img'
+          src={book.imageLinks.smallThumbnail}
+          alt=''
+        />
+      </div>
 
-class Book extends Component {
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    onUpdateBook: PropTypes.func.isRequired
-  }
-
-  state = {
-    isClicked: false
-  }
-
-  render() {
-    const { book, onUpdateBook } = this.props;
-
-    return (
-      <section className='book-info'>
-        <div className='cover-container'>
-          <img
-            className='cover-img'
-            src={book.imageLinks.smallThumbnail}
-            alt=''
-          />
-          {/* <img
-            src={`${dropdown}`}
-            alt=''
-            className='dropdown-icon'
-            onClick={() => this.setState({ isClicked: !this.state.isClicked })}
-          />
-          {this.state.isClicked && <DropDown book={book} onUpdateBook={onUpdateBook}/>} */}
-          
-        </div>
-
-        <h2 className='book-title'>{book.title}</h2>
-        {/* {book.subtitle && <p>Subtitle: {book.subtitle}</p>} */}
-        <p><strong>Authors:</strong> {book.authors.join(', ')}</p>
-        {/* <p><strong>Rate:</strong> {book.averageRating || "No rating"}</p> */}
-        {/* <p>Shelf: {book.shelf}</p> */}
-        <DropDown book={book} onUpdateBook={onUpdateBook} />
-      </section>
-    )
-  }
+      <h2 className='book-title'>{book.title}</h2>
+      <p><strong>Authors:</strong> {book.authors.join(', ')}</p>
+      <DropDown book={book} onUpdateBook={onUpdateBook} />
+    </section>
+  )
 }
 
-// function Book({ book, onUpdateBook }) {
-//   return (
-//     <div className='book-info'>
-//       <div className='cover-container'>
-//         <img src={book.imageLinks.smallThumbnail} alt='' />
-//         <img src={`${dropdown}`} alt='' className='dropdown-icon' onClick={onUpdateBook} />
-//         <DropDown />
-//       </div>
-      
-//       <h2 className='book-title'>{book.title}</h2>
-//       {/* {book.subtitle && <p>Subtitle: {book.subtitle}</p>} */}
-//       <p><strong>Authors:</strong> {book.authors}</p>
-//       <p><strong>Rate::</strong> {book.averageRating || "No rating"}</p>
-//       <p>Shelf: {book.shelf}</p>
-//     </div>
-//   )
-// }
-
 export default Book;
+
+Book.propTypes = {
+  book: PropTypes.object.isRequired,
+  onUpdateBook: PropTypes.func.isRequired
+}
