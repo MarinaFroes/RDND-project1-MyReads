a15519913bc75e58539fba1d80ee89ed7d1a879b
MFAC <ninafroes@gmail.com>
2019-11-03 14:32:57 +0100

    feat: Add Link to searchPage

--- src/Components/SearchIcon.js
+++ src/Components/SearchIcon.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function SearchIcon() {
   return (
-    <>
+    <Link
+      to='/search'
+    >
       <div className='search-icon'></div>
-    </>
+    </Link>
   )
 }
+
 
 export default SearchIcon;
