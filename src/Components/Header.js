5e465e4b07209e9b46754c9044305c839850316e
MFAC <ninafroes@gmail.com>
2019-11-03 21:04:23 +0100

    feat: Add link to home page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { FaBookReader } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function Header({ title }) {
   return (
     <header>
       <div className='header-icon'><FaBookReader /></div>
-      <h1>{title}</h1>
+      <Link to='/' className='app-title'>
+        <h1>{title}</h1>
+      </Link>
     </header>
   )
 }
