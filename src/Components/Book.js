212d6e764827df8ce07e58e1213fbf8a695d5d3b
MFAC <ninafroes@gmail.com>
2019-11-03 12:15:50 +0100

    feat: Remove dropdown icon from Book component

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,7 +1,7 @@
 // import React from 'react'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import dropdown from '../icons/dropdown.svg'
+// import dropdown from '../icons/dropdown.svg'
 import DropDown from './DropDown'
 
 
@@ -26,20 +26,22 @@ class Book extends Component {
             src={book.imageLinks.smallThumbnail}
             alt=''
           />
-          <img
+          {/* <img
             src={`${dropdown}`}
             alt=''
             className='dropdown-icon'
             onClick={() => this.setState({ isClicked: !this.state.isClicked })}
           />
-          {this.state.isClicked && <DropDown book={book} onUpdateBook={onUpdateBook}/>}
+          {this.state.isClicked && <DropDown book={book} onUpdateBook={onUpdateBook}/>} */}
+          
         </div>
 
         <h2 className='book-title'>{book.title}</h2>
         {/* {book.subtitle && <p>Subtitle: {book.subtitle}</p>} */}
-        <p><strong>Authors:</strong> {book.authors}</p>
+        <p><strong>Authors:</strong> {book.authors.join(', ')}</p>
         {/* <p><strong>Rate:</strong> {book.averageRating || "No rating"}</p> */}
-        <p>Shelf: {book.shelf}</p>
+        {/* <p>Shelf: {book.shelf}</p> */}
+        <DropDown book={book} onUpdateBook={onUpdateBook} />
       </div>
     )
   }
