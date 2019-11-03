0673e02702ca27a62bdb10982fe6fb6504149a7f
MFAC <ninafroes@gmail.com>
2019-11-03 15:34:17 +0100

    feat: Add icon to Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { FaBookReader } from 'react-icons/fa'
 
 function Header({ title }) {
   return (
     <header>
+      <div className='header-icon'><FaBookReader /></div>
+      
       <h1>{title}</h1>
     </header>
   )
