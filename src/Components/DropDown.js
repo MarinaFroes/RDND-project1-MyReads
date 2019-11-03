5cb381bc2b207294413b24a7a135479bebfbc7bf
MFAC <ninafroes@gmail.com>
2019-11-03 20:43:17 +0100

    feat: Add currentShelf prop

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -2,20 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { SHELVES_TYPES } from '../utils/SHELVES_TYPES'
 
-function DropDown({ book, onUpdateBook }) {
+function DropDown({ book, onUpdateBook, currentShelf }) {
   return (
     <div className='drop-down-container'>
       <select
         className='drop-down-menu'
         role="menubar"
         onChange={event => onUpdateBook(book, event.target.value)}
+        value={currentShelf}
       >
         {SHELVES_TYPES.map(shelf => ( 
           <option
             key={shelf[1]}
             value={shelf[1]}
             role='menuitem'
-            selected={book.shelf === shelf[1]}
           >
             {shelf[0]}
           </option>
@@ -29,5 +29,6 @@ export default DropDown;
 
 DropDown.propTypes = {
   book: PropTypes.object.isRequired,
-  onUpdateBook: PropTypes.func.isRequired
+  onUpdateBook: PropTypes.func.isRequired,
+  currentShelf: PropTypes.string.isRequired
 }
