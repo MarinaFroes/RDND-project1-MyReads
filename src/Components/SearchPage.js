ebea2a700de357f12257c948196965328baccca3
MFAC <ninafroes@gmail.com>
2019-11-04 16:11:40 +0100

    feat: Add onClearQuery prop

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -2,11 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import SearchBar from './SearchBar'
 
-function SearchPage({query, onUpdateQuery}) {
+function SearchPage({query, onUpdateQuery, onClearQuery}) {
   return (
     <SearchBar
       query={query}
       onUpdateQuery={onUpdateQuery}
+      onClearQuery={onClearQuery}
     /> 
   )
 }
@@ -15,5 +16,6 @@ export default SearchPage
 
 SearchPage.propTypes = {
   query: PropTypes.string.isRequired,
-  onUpdateQuery: PropTypes.func.isRequired
+  onUpdateQuery: PropTypes.func.isRequired,
+  onClearQuery: PropTypes.func.isRequired
 }
