0c4a816be613dadd7347e8bdcb3cf0d1e6768f2f
MFAC <ninafroes@gmail.com>
2019-11-04 16:12:18 +0100

    feat: Remove link to home page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { FaBookReader } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
 
 function Header({ title }) {
   return (
     <header>
       <div className='header-icon'><FaBookReader /></div>
-      <Link to='/' className='app-title'>
-        <h1>{title}</h1>
-      </Link>
+      <h1>{title}</h1>
     </header>
   )
 }
