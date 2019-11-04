defcbfcfe78e5887788d4f4380a6243642e44108
MFAC <ninafroes@gmail.com>
2019-11-04 20:46:03 +0100

    feat: Add show search results

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import SearchBar from './SearchBar'
+import Book from './Book'
 
-function SearchPage({query, onUpdateQuery, onClearQuery}) {
+function SearchPage({query, onUpdateQuery, onClearQuery, showingBooks, onUpdateBook}) {
   return (
-    <SearchBar
-      query={query}
-      onUpdateQuery={onUpdateQuery}
-      onClearQuery={onClearQuery}
-    /> 
+    <>
+      <SearchBar
+        query={query}
+        onUpdateQuery={onUpdateQuery}
+        onClearQuery={onClearQuery}
+      /> 
+      {showingBooks.length > 0 && (
+        <>
+          {console.log(showingBooks)}
+          <p>Found <strong>{showingBooks.length}</strong> results.</p>
+          <div className='show-search-results'>
+            {showingBooks.map(book =>
+              <Book key={book.id} book={book} onUpdateBook={onUpdateBook} currentShelf='none' />
+            )}
+          </div>
+        </>
+      )}
+    </>
   )
 }
 
@@ -17,5 +31,7 @@ export default SearchPage
 SearchPage.propTypes = {
   query: PropTypes.string.isRequired,
   onUpdateQuery: PropTypes.func.isRequired,
-  onClearQuery: PropTypes.func.isRequired
+  onClearQuery: PropTypes.func.isRequired,
+  onUpdateBook: PropTypes.func.isRequired,
+  showingBooks: PropTypes.array.isRequired
 }
