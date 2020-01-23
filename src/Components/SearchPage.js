2123186f5e4f2167b3e32e074387fa5183dec7e0
MFAC <ninafroes@gmail.com>
2020-01-23 16:28:02 +0100

    feat: Add shelves to Book on SearchPage

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import SearchBar from './SearchBar'
 import Book from './Book'
 
-function SearchPage({query, onUpdateQuery, onClearQuery, searchedBooks, books, onUpdateBook}) {
+function SearchPage({ query, onUpdateQuery, onClearQuery, searchedBooks, books, onUpdateBook }) {
   return (
     <main className="search-page">
       <SearchBar
@@ -19,14 +19,25 @@ function SearchPage({query, onUpdateQuery, onClearQuery, searchedBooks, books, o
 
       {searchedBooks.length > 0 && (
         <div className="show-search-results">
-          {searchedBooks.map(book => (
-            <Book
-              key={book.id}
-              book={book}
-              onUpdateBook={onUpdateBook}
-              currentShelf={book.shelf || "none"}
-            />
-          ))}
+          {
+            searchedBooks.map(searchedBook => {
+              
+              books.map(book => {
+                  if (book.id === searchedBook.id) {
+                    searchedBook.shelf = book.shelf
+                  }
+              }) 
+              
+              return (
+                <Book
+                  key={searchedBook.id}
+                  book={searchedBook}
+                  onUpdateBook={onUpdateBook}
+                  currentShelf={searchedBook.shelf || "none"}
+                />
+              )
+            })
+          }
         </div>
       )}
     </main>
