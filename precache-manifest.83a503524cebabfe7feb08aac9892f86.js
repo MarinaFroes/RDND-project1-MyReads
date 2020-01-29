690b32573ab6735eeda8c468ec82fee0222438dd
MFAC <ninafroes@gmail.com>
2020-01-29 21:50:19 +0100

    Updates

---
+++ precache-manifest.83a503524cebabfe7feb08aac9892f86.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f40d08fdfb00230a760d171264351f0",
+    "url": "/RDND-project1-MyReads/index.html"
+  },
+  {
+    "revision": "ca85cc007cf0a3adb11c",
+    "url": "/RDND-project1-MyReads/static/css/main.81a92611.chunk.css"
+  },
+  {
+    "revision": "4d8f1361f05fe8e8a9a8",
+    "url": "/RDND-project1-MyReads/static/js/2.b138b847.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/RDND-project1-MyReads/static/js/2.b138b847.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca85cc007cf0a3adb11c",
+    "url": "/RDND-project1-MyReads/static/js/main.1bc68c03.chunk.js"
+  },
+  {
+    "revision": "0fb07e79b41d8d7acdbf",
+    "url": "/RDND-project1-MyReads/static/js/runtime-main.9a127eb0.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/RDND-project1-MyReads/static/media/arrowback.3687aa98.svg"
+  },
+  {
+    "revision": "dd2a802be095cc2b328ee0a2f39b1fef",
+    "url": "/RDND-project1-MyReads/static/media/magnifyingglass.dd2a802b.svg"
+  },
+  {
+    "revision": "ce2bfb5989dc2a5620bb1de338e03e60",
+    "url": "/RDND-project1-MyReads/static/media/mylogo.ce2bfb59.svg"
+  }
+]);
